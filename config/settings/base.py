349f2ee9f67886ed31accdca64c359855d925b66
William Fry <wrfry95@gmail.com>
2018-01-10 18:31:00 -0500

    Avoid relative imports

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 if os.environ.get('ENVIRONMENT') == 'PROD':
-    from .production import *
+    from config.settings.production import *
 else:
-    from .local import *
+    from config.settings.local import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
