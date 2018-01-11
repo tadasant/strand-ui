c8ec36f8805677060b4e2398d3964097dc47f434
William Fry <wrfry95@gmail.com>
2018-01-10 20:43:53 -0500

    Fix admin

--- app/groups/admin.py
+++ app/groups/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
-from app.slack.models import SlackChannel, SlackSettings, SlackTeam, SlackUser
+from app.slack.models import Group, GroupSettings
 
-admin.site.register(SlackTeam)
-admin.site.register(SlackSettings)
-admin.site.register(SlackUser)
-admin.site.register(SlackChannel)
+admin.site.register(Group)
+admin.site.register(GroupSettings)
