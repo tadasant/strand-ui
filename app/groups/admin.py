cb37af34189394059dcb3893d1e3ce86b048fbec
William Fry <wrfry95@gmail.com>
2018-01-10 20:44:22 -0500

    Fix typo in groups admin.py

--- app/groups/admin.py
+++ app/groups/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from app.slack.models import Group, GroupSettings
+from app.groups.models import Group, GroupSettings
 
 admin.site.register(Group)
 admin.site.register(GroupSettings)
