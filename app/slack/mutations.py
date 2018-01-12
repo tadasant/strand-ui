28e2e9d212adaa1c88a180cd329c9781450e1b8c
William Fry <wrfry95@gmail.com>
2018-01-12 16:40:41 -0500

    Completed initial slack mutations

--- app/slack/mutations.py
+++ app/slack/mutations.py
@@ -1,7 +1,17 @@
 import graphene
-from app.slack.types import SlackUserType, SlackUserInputType
-from app.slack.models import SlackTeam, SlackUser
+from app.slack.types import (
+    SlackUserType,
+    SlackUserInputType,
+    SlackTeamType,
+    SlackTeamInputType,
+    SlackChannelType,
+    SlackChannelInputType,
+    SlackSettingsType,
+    SlackSettingsInputType
+)
+from app.slack.models import SlackTeam, SlackUser, SlackChannel, SlackSettings
 from app.users.models import User
+from app.groups.models import Group
 
 
 class CreateSlackUserMutation(graphene.Mutation):
@@ -10,7 +20,7 @@ class CreateSlackUserMutation(graphene.Mutation):
 
     status = graphene.Int()
     form_errors = graphene.String()
-    slack_user = graphene.Field(lambda: SlackUserType)
+    slack_user = graphene.Field(SlackUserType)
 
     def mutate(self, info, input):
         if not info.context.user.is_authenticated:
@@ -24,8 +34,71 @@ class CreateSlackUserMutation(graphene.Mutation):
                                            form_errors={'message': ['Invalid User Id']})
 
         slack_user = SlackUser.objects.create(**input)
-        return CreateSlackUserMutation(slack_user=slack_user)
+        return CreateSlackUserMutation(slack_user=slack_user, status=201)
+
+
+class CreateSlackTeamMutation(graphene.Mutation):
+    class Arguments:
+        input = SlackTeamInputType(required=True)
+
+    status = graphene.Int()
+    form_errors = graphene.String()
+    slack_team = graphene.Field(SlackTeamType)
+
+    def mutate(self, info, input):
+        if not info.context.user.is_authenticated:
+            return CreateSlackTeamMutation(status=403)
+
+        if not Group.objects.filter(pk=input.group_id).exists():
+            return CreateSlackTeamMutation(status=400,
+                                           form_errors={'message': ['Invalid Group Id']})
+
+        slack_team = SlackTeam.objects.create(**input)
+        return CreateSlackTeamMutation(slack_team=slack_team, status=201)
+
+
+class CreateSlackChannelMutation(graphene.Mutation):
+    class Arguments:
+        input = SlackChannelInputType(required=True)
+
+    status = graphene.Int()
+    form_errors = graphene.String()
+    slack_channel = graphene.Field(SlackChannelType)
+
+    def mutate(self, info, input):
+        if not info.context.user.is_authenticated:
+            return CreateSlackChannelMutation(status=403)
+
+        if not SlackTeam.objects.filter(pk=input.slack_team_id).exists():
+            return CreateSlackChannelMutation(status=400,
+                                              form_errors={'message': ['Invalid Slack Team Id']})
+
+        slack_channel = SlackChannel.objects.create(**input)
+        return CreateSlackChannelMutation(slack_channel=slack_channel, status=201)
+
+
+class CreateSlackSettingsMutation(graphene.Mutation):
+    class Arguments:
+        input = SlackSettingsInputType(required=True)
+
+    status = graphene.Int()
+    form_errors = graphene.String()
+    slack_settings = graphene.Field(SlackSettingsType)
+
+    def mutate(self, info, input):
+        if not info.context.user.is_authenticated:
+            return CreateSlackSettingsMutation(status=403)
+
+        if not SlackTeam.objects.filter(pk=input.slack_team_id).exists():
+            return CreateSlackSettingsMutation(status=400,
+                                               form_errors={'message': ['Invalid Slack Team Id']})
+
+        slack_settings = SlackSettings.objects.create(**input)
+        return CreateSlackSettingsMutation(slack_settings=slack_settings, status=201)
 
 
 class Mutation(graphene.ObjectType):
     create_slack_user = CreateSlackUserMutation.Field()
+    create_slack_team = CreateSlackTeamMutation.Field()
+    create_slack_channel = CreateSlackChannelMutation.Field()
+    create_slack_settings = CreateSlackSettingsMutation.Field()
--- app/slack/types.py
+++ app/slack/types.py
@@ -9,22 +9,6 @@ class SlackUserType(DjangoObjectType):
         model = SlackUser
 
 
-class SlackUserInputType(graphene.InputObjectType):
-    id = graphene.String(required=True)
-    first_name = graphene.String()
-    last_name = graphene.String()
-    real_name = graphene.String(required=True)
-    display_name = graphene.String(required=True)
-    email = graphene.String()
-    avatar_72 = graphene.String()
-    is_bot = graphene.Boolean(required=True)
-    is_admin = graphene.Boolean(required=True)
-
-    slack_team_id = graphene.String(required=True)
-    user_id = graphene.Int(required=True)
-
-
-
 class SlackChannelType(DjangoObjectType):
     class Meta:
         model = SlackChannel
@@ -38,3 +22,35 @@ class SlackTeamType(DjangoObjectType):
 class SlackSettingsType(DjangoObjectType):
     class Meta:
         model = SlackSettings
+
+
+class SlackUserInputType(graphene.InputObjectType):
+    id = graphene.String(required=True)
+    first_name = graphene.String()
+    last_name = graphene.String()
+    real_name = graphene.String(required=True)
+    display_name = graphene.String(required=True)
+    email = graphene.String()
+    avatar_72 = graphene.String()
+    is_bot = graphene.Boolean(required=True)
+    is_admin = graphene.Boolean(required=True)
+    slack_team_id = graphene.String(required=True)
+    user_id = graphene.Int(required=True)
+
+
+class SlackTeamInputType(graphene.InputObjectType):
+    id = graphene.String(required=True)
+    name = graphene.String(required=True)
+    group_id = graphene.Int(required=True)
+
+
+class SlackChannelInputType(graphene.InputObjectType):
+    id = graphene.String(required=True)
+    name = graphene.String(required=True)
+    slack_team_id = graphene.String(required=True)
+    session_id = graphene.Int(required=True)
+
+
+class SlackSettingsInputType(graphene.InputObjectType):
+    bot_token = graphene.String()
+    slack_team_id = graphene.Int(required=True)
