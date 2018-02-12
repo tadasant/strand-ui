c3d8953775fa9760d1b0b5d7e94b03d6d9d8ea78
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-12 11:38:48 -0500

    env files and apollo integration

--- client/index.js
+++ client/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloProvider} from 'react-apollo';
 import Root from './src/Root';
 
-ReactDOM.render(<Root/>, document.getElementById('root'));
+// Last of config setup
+GraphQLURL = process.env.PORTAL_GRAPHQL_URL;
+if (process.env.REALM === 'staging') {
+  // Staging build should mirror production except...
+  GraphQLURL = 'https://www.staging.codeclippy.com/graphql';
+}
+
+const client = new ApolloClient({
+  link: new HttpLink({uri: GraphQLURL}),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Root/>
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
