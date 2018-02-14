3c1441a6ed3056ee8b3fbec510b3fc798d7acca6
William Fry <wrfry95@gmail.com>
2018-02-14 17:52:35 -0500

    Use SSL in staging and production

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -23,6 +23,11 @@ DATABASES = {
 }
 
 ENABLE_GRAPHIQL = True
+
+# SSL/HTTPS
+# https://docs.djangoproject.com/en/2.0/topics/security/
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 
 # Slack credentials
