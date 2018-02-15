8782529f2bf5f3a7f39fc3b08bbb73c97122f880
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-15 11:10:36 -0500

    refactored to make mocking resolvers possible

--- client/test/helper/applicationMock.js
+++ client/test/helper/applicationMock.js
@@ -4,14 +4,28 @@ import App from 'src/App.react';
 import {MemoryRouter} from 'react-router';
 import {ApolloProvider} from 'react-apollo';
 import PropTypes from 'prop-types';
+import {addMockFunctionsToSchema} from 'graphql-tools';
+import {SchemaLink} from 'apollo-link-schema';
+import * as graphqlIntrospectionResult from '../schema';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {buildClientSchema} from 'graphql';
+import {ApolloClient} from 'apollo-client';
+import get from 'lodash/get';
 
 
 /*
   Mount the entire application (minus stuff in Root.react.js).
+
+  (optional) options: {
+    graphqlMocks: {
+      <TypeName>: <Response>
+    }
+  }
  */
-export const mountApplication = (endpoint) => {
+export const mountApplication = (endpoint, options) => {
+  const mockApolloClient = generateMockApolloClient(get(options, 'graphqlMocks'));
   return mount(
-    <ApolloProvider client={global.__MOCK_APOLLO_CLIENT__}>
+    <ApolloProvider client={mockApolloClient}>
       <MemoryRouter initialEntries={[endpoint]} initialIndex={0} keyLength={0}>
         {/*keyLength is 0 so keys aren't generated (they break jest snapshots)*/}
         <App/>
@@ -22,4 +36,17 @@ export const mountApplication = (endpoint) => {
       childContextTypes: {uiHost: PropTypes.string}
     }
   );
+};
+
+const generateMockApolloClient = (graphqlMocks) => {
+  const schema = buildClientSchema(graphqlIntrospectionResult.data);
+  addMockFunctionsToSchema({
+    schema,
+    mocks: graphqlMocks || {}
+  });
+  const apolloCache = new InMemoryCache(window.__APOLLO_STATE__);
+  return new ApolloClient({
+    cache: apolloCache,
+    link: new SchemaLink({schema}),
+  });
 };
--- client/test/setup.js
+++ client/test/setup.js
@@ -1,28 +1,9 @@
 import Adapter from 'enzyme-adapter-react-16';
 import {configure} from 'enzyme';
 import dotenv from 'dotenv'
-import {buildClientSchema} from 'graphql';
-import * as graphqlIntrospectionResult from 'test/schema.json';
-import {addMockFunctionsToSchema} from 'graphql-tools';
-import {ApolloClient} from 'apollo-client';
-import {SchemaLink} from 'apollo-link-schema';
-import {InMemoryCache} from 'apollo-cache-inmemory';
 
 // environment variables
 dotenv.config();
 
 // enzyme
 configure({adapter: new Adapter()});
-
-// graphql mocking
-const schema = buildClientSchema(graphqlIntrospectionResult.data);
-addMockFunctionsToSchema({schema, mocks: {}, preserveResolvers: true});
-const apolloCache = new InMemoryCache(window.__APOLLO_STATE__);
-const client = new ApolloClient({
-  cache: apolloCache,
-  link: new SchemaLink({schema}),
-});
-
-global.__MOCK_APOLLO_CLIENT__ = client;
-
-// TODO figure out how to run these once instead of once before each test suite
