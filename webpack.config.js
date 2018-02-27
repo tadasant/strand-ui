0de1b004d427e203289f0e67dd1e11898c3d0161
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-27 18:46:24 -0500

    basic webpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  entry: './index.js',
+};
+
+module.exports = config;
