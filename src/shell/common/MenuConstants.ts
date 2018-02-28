05bee8348fd7199bbbc204809fde45cce1d275b1
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-28 10:32:50 -0500

    menuconstants.ts

---
+++ src/shell/common/MenuConstants.ts
@@ -0,0 +1,9 @@
+interface NavigationPaths {
+  [label: string] : string,
+}
+
+export const navigationLabelToPath: NavigationPaths = {
+  Install: '/install',
+};
+
+export default {navigationLabelToPath};
