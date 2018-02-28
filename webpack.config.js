1094626774f9802500199b9345e7b78c88732102
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-27 19:20:58 -0500

    local dev server working

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const HTMLPlugin = require('html-webpack-plugin');
+
 const config = {
+  entry: './index.js',
   module: {
     rules: [
       {
@@ -19,7 +22,15 @@ const config = {
       },
     ],
   },
-  entry: './index.js',
+  devServer: {
+    contentBase: './',
+    port: 3000,
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
 
 module.exports = config;
