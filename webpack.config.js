5e26973a500b4ff404d815101c033ee63e7ecdfe
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-27 19:58:55 -0500

    first pass at hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HTMLPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 const config = {
   entry: './index.js',
@@ -23,15 +25,20 @@ const config = {
       },
     ],
   },
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: './',
     port: 3000,
+    hot: true,
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HTMLPlugin({
       template: 'index.html',
     }),
     new Dotenv(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
 
