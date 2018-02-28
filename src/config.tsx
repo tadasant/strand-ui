1ac3e8521bbf8cfe4630316fb6fdf6a7d56d90ae
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-28 12:03:10 -0500

    moved config to src

--- src/config.tsx
+++ src/config.tsx
@@ -4,5 +4,5 @@ export const UI_HOST = process.env.UI_HOST;
 export const SLACK_SCOPES = process.env.SLACK_SCOPES;
 export const NODE_ENV = process.env.NODE_ENV;
 export const VERSION = process.env.VERSION;
-export const SENTRY_KEY = process.env.SENTRY_KEY;
+// export const SENTRY_KEY = process.env.SENTRY_KEY;
 export const SENTRY_PROJECT_ID = process.env.SENTRY_PROJECT_ID;
