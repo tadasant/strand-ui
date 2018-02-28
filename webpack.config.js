9e9545b04186905807228b80e8e2b1882b5e0a79
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-02-28 12:05:14 -0500

    UI-45: Parcel to webpack (#5)
    * basic webpack bundler
    
    * local dev server working
    
    * added dotenv support
    
    * first pass at hmr
    
    * handling react router for webpack
    
    * working hmr
    
    * moving index config to env vars
    
    * working public path
    
    * repaired local public out
    
    * removed parcel stuff from circleci

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HTMLPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+  // Root JS file for bundling
+  entry: './index.js',
+  module: {
+    rules: [
+      // Use babel for transpiling ES6
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      // Bundle files (e.g. images)
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  // Enable served source maps
+  devtool: 'inline-source-map',
+  // Webpack Dev Server for running locally
+  devServer: {
+    // Play nicely with react-router
+    historyApiFallback: true,
+    port: 3000,
+    // Enable hot module reloading (HMR)
+    hot: true,
+  },
+  plugins: [
+    // Cleans the build folder per-build/reload
+    new CleanWebpackPlugin(['dist']),
+    // Builds the .html file for entering into bundle
+    new HTMLPlugin({
+      template: 'index.html',
+    }),
+    // Integrate .env files
+    new Dotenv(),
+    // HMR plugins
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
+
+module.exports = config;
